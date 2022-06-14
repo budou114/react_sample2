6fb9eb1028269e480862736a17ca6fd43432a8b1
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-14 09:20:51 +0900

    Styled JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 };
